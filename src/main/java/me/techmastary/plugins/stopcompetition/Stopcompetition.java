52e2a9ab2e48db02611fd2b64a82c681536de6d0
hkminegod <choyiny@gmail.com>
2013-08-10 22:19:03 +0800

    Updated code

--- src/main/java/me/techmastary/plugins/stopcompetition/Stopcompetition.java
+++ src/main/java/me/techmastary/plugins/stopcompetition/Stopcompetition.java
@@ -22,7 +22,7 @@ public class Stopcompetition extends JavaPlugin implements Listener {
 	public int basePlayers;
 	public String prefix = "prefix";
 	public boolean executedCommand;
-	public boolean gameEnabled;
+	public boolean stopGameEnabled;
 
 	public void onDisable() {
 	}
@@ -33,12 +33,10 @@ public class Stopcompetition extends JavaPlugin implements Listener {
 		System.out.println(plugin.getName() + " version " + plugin.getVersion() + " enabled.");
 		getConfig().options().copyDefaults(true);
 		this.saveDefaultConfig();
-		stopTime = getConfig().getInt("DelayStopCommand");
-		notify = getConfig().getBoolean("NotifyPlayers");
 		cmds = getConfig().getString("Commands");
 		prefix = getConfig().getString("Prefix");
 		executedCommand = false;
-		gameEnabled = false;
+		stopGameEnabled = false;
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
@@ -48,22 +46,27 @@ public class Stopcompetition extends JavaPlugin implements Listener {
 		}
 		if (commandLabel.equalsIgnoreCase("startstopcompetition")) {
 			if (sender.hasPermission("stop.admin")) {
-				gameEnabled = true;
+				stopGameEnabled = true;
 			}
 		}
 		if (commandLabel.equalsIgnoreCase("ssc")) {
 			if (sender.hasPermission("stop.admin")) {
-				gameEnabled = true;
-				Bukkit.broadcastMessage("The game has started! Type /stop right now. The person who types it will win the game!");
+				stopGameEnabled = true;
+				Bukkit.broadcastMessage("The game has started! Type /stop right now. The first person who types it will win the game!");
 			}
 		}
 		if (commandLabel.equalsIgnoreCase("stop")) {
-			if (gameEnabled) {
+			if (stopGameEnabled) {
 				if (executedCommand == false) {
 					executedCommand = true;
 					sender.sendMessage(ChatColor.GREEN + "" + prefix + ChatColor.GRAY + "You are the first person to type /stop!");
 					Bukkit.broadcastMessage(ChatColor.GRAY + "" + sender.getName() + " won the game. Server is now shutting down ^^.");
 					Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), getConfig().getString(cmds).replace("%player%", sender.getName()));
+					this.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+						public void run() {
+							Bukkit.shutdown();
+						}
+					}, 100L);
 					Bukkit.shutdown();
 					return true;
 				} else {
